32fd3c6e868417b00095b234beb4a3c779c0fd0e
Simone Lippolis <213991+leeppolis@users.noreply.github.com>
2021-09-16 14:51:21 +0000

    Improvements

--- scripts/smallmultiples.js
+++ scripts/smallmultiples.js
@@ -3,8 +3,19 @@ const smallMultiples = (years, data) => {
   let maxMax = Number.MIN_SAFE_INTEGER;
   let minMin = Number.MAX_SAFE_INTEGER;
   const chartHeight = 220;
+  let chartWidth = 340;
   const margin = 80;
   const vMargin = 40;
+
+  if (window.matchMedia('screen and (min-width:768px)').matches) {
+    chartWidth = 340;
+  } else if (window.matchMedia('screen and (min-width:1024px)').matches) {
+    chartWidth = 460;
+  } else if (window.matchMedia('screen and (min-width:1366px)').matches) {
+    chartWidth = 380;
+  }
+
+
   years.forEach((y, i) => {
     data.auto[y].forEach((c, i) => {
       if (!smallMultiples[c.classe]) {
@@ -19,10 +30,8 @@ const smallMultiples = (years, data) => {
       }
     });
   });
-  console.log(smallMultiples, minMin, maxMax);
   const classes = Object.keys(smallMultiples);
   classes.sort().reverse();
-  console.log(classes);
 
   const wrapper = d3.select('#small-m');
   classes.forEach((c) => {
@@ -41,7 +50,7 @@ const smallMultiples = (years, data) => {
 
 
     const scaleX = d3.scaleLinear()
-      .range([margin, 420 - (margin)])
+      .range([margin, chartWidth - (margin)])
       .domain([0, 12]);
     const scaleY = d3.scaleLinear()
       .range([chartHeight - (vMargin), vMargin])
@@ -59,14 +68,16 @@ const smallMultiples = (years, data) => {
       .curve(d3.curveCatmullRom.alpha(0.5));
     const data = smallMultiples[c].map((d, i) => ({ x: scaleX(i), y: scaleY(d)}));
 
+    console.log('--', chartWidth);
+
     const svgS = container.append('svg')
-      .attr('width', 420)
+      .attr('width', chartWidth)
       .attr('height', chartHeight)
-      .attr('viewbox', `0 0 420 180`)
+      .attr('viewbox', `0 0 chartWidth 180`)
       .attr('preserveAspectRatio', 'xMidYMid meet');
     svgS.append('line')
       .attr('x1', margin)
-      .attr('x2', 420 - (margin))
+      .attr('x2', chartWidth - (margin))
       .attr('y1', chartHeight - vMargin)
       .attr('y2', chartHeight - vMargin)
       .attr('stroke', colors.NEUTRAL)
@@ -79,14 +90,14 @@ const smallMultiples = (years, data) => {
       .attr('stroke', colors.NEUTRAL)
       .attr('fill', 'none');
     svgS.append('line')
-      .attr('x1', 420 - (margin))
-      .attr('x2', 420 - (margin))
+      .attr('x1', chartWidth - (margin))
+      .attr('x2', chartWidth - (margin))
       .attr('y1', chartHeight - vMargin)
       .attr('y2', chartHeight - vMargin + 10)
       .attr('stroke', colors.NEUTRAL)
       .attr('fill', 'none');
     svgS.append('text')
-      .attr('x', 420 - (margin))
+      .attr('x', chartWidth - (margin))
       .attr('y', chartHeight - vMargin + 20)
       .attr('class', 'axis-label')
       .attr('text-anchor', 'end')
@@ -100,7 +111,7 @@ const smallMultiples = (years, data) => {
       .attr('alignment-baseline', 'top')
       .text('2007');
     svgS.append('text')
-      .attr('x', 210)
+      .attr('x', chartWidth / 2)
       .attr('y', chartHeight - vMargin + 20)
       .attr('class', 'axis-label')
       .attr('text-anchor', 'middle')
@@ -109,20 +120,16 @@ const smallMultiples = (years, data) => {
 
     const min = smallMultiples[c][0];
     const max = smallMultiples[c][smallMultiples[c].length - 1];
-
     let string = (min < max) ? `+${formatNumber(Math.round(max / min))}×` : `-${formatNumber(Math.round(min / max))}×`;
-
-    console.log(scaleY(min), scaleY(max));
     if (string === '-1×' || string === '+1×') {
-      string = '≃';
+      string = '';
+    } else if (string === '+Infinity×') {
+      string = '';
     }
     const ypos = (Math.abs(scaleY(min) - scaleY(max)) > 15) ? scaleY(d3.mean([min, max])) : scaleY(d3.mean([min, max])) - 15;
-    console.log(scaleY(min), scaleY(max), '=', ypos );
-
-
     svgS.append('line')
       .attr('x1', (margin))
-      .attr('x2', 420 - (margin))
+      .attr('x2', chartWidth - (margin))
       .attr('y1', scaleY(min))
       .attr('y2', scaleY(min))
       .attr('stroke', colors.NEUTRAL)
@@ -132,7 +139,7 @@ const smallMultiples = (years, data) => {
 
     svgS.append('line')
       .attr('x1', (margin))
-      .attr('x2', 420 - (margin))
+      .attr('x2', chartWidth - (margin))
       .attr('y1', scaleY(max))
       .attr('y2', scaleY(max))
       .attr('stroke', colors.NEUTRAL)
@@ -140,12 +147,32 @@ const smallMultiples = (years, data) => {
       .attr('stroke-dasharray', '2 1')
       .attr('fill', 'none');
 
+      svgS.append('line')
+        .attr('x1', (margin))
+        .attr('x2', chartWidth - (margin))
+        .attr('y1', scaleY(maxMax))
+        .attr('y2', scaleY(maxMax))
+        .attr('stroke', colors.NEUTRAL)
+        .attr('stroke-width', 1)
+        .attr('stroke-dasharray', '2 4 1 4')
+        .attr('fill', 'none');
+
+      svgS.append('text')
+        .attr('x', margin)
+        .attr('dx', -5)
+        .attr('y', scaleY(maxMax))
+        .attr('class', 'numbers')
+        .attr('text-anchor', 'end')
+        .attr('alignment-baseline', 'middle')
+        .text(formatNumber(maxMax));
+
     svgS.append('path')
       .attr('d', area(data))
       .attr('class', `sm-area sm-area-${c}`)
       .attr('id', `sm-area-${c}`)
       .attr('stroke', 'none')
       .attr('fill', 'url(#fill)');
+
     svgS.append('path')
       .attr('d', line(data))
       .attr('class', `sm-line sm-line-${c}`)
@@ -155,7 +182,7 @@ const smallMultiples = (years, data) => {
       .attr('fill', 'none');
 
     svgS.append('text')
-      .attr('x', 210)
+      .attr('x', chartWidth / 2)
       .attr('y', ypos)
       .attr('class', 'annotation')
       .attr('text-anchor', 'middle')
@@ -177,7 +204,7 @@ const smallMultiples = (years, data) => {
       .attr('class', 'numbers')
       .attr('text-anchor', 'start')
       .attr('alignment-baseline', 'middle')
-      .text('Veicoli');
+      .text('🚘');
 
     svgS.append('text')
       .attr('x', data[data.length - 1].x)
