31dbcdace1ebed057d06006c972106ac877da3c3
Simone Lippolis <213991+leeppolis@users.noreply.github.com>
2021-09-27 15:32:27 +0000

    New Rain

--- lagrandecorsa/scripts/rain.js
+++ lagrandecorsa/scripts/rain.js
@@ -1,80 +1,100 @@
 
 const rain = (svgW, years, pioggia) => {
-  const svgL = d3.select('#rain')
-    .append('svg')
-    .attr('width', svgW)
-    .attr('height', lineChartHeight)
-    .attr('viewbox', `0 0 ${svgW} 200`)
-    .attr('preserveAspectRatio', 'xMidYMid meet');
+  const charts = [
+    {
+      title: '<span style="color:#0084C4">mm</span> di pioggia',
+      unit: 'mm',
+      id: 'mm',
+      label: 'mm',
+      path: 'mm',
+    },
+    {
+      title: '<span style="color:#0084C4">giorni</span> di pioggia',
+      unit: 'giorni',
+      id: 'dd',
+      label: 'giorni',
+      path: 'dd',
+    }
+  ];
 
-  const gL = svgL.append('g');
-  const gG = svgL.append('g');
-  const gD = svgL.append('g');
-  const gLB = svgL.append('g');
+  charts.forEach((c) => {
+    const cc = d3.select('#rain')
+      .append('div')
+      .attr('class', 'chart-container');
+     const svgL = cc.append('svg')
+      .attr('width', svgW)
+      .attr('height', lineChartHeight)
+      .attr('viewbox', `0 0 ${svgW} 200`)
+      .attr('preserveAspectRatio', 'xMidYMid meet');
 
-  const data = [];
+    const gL = svgL.append('g');
+    const gG = svgL.append('g');
+    const gD = svgL.append('g');
+    const gLB = svgL.append('g');
 
-  years.forEach((y, i) => {
-    data.push(pioggia[y])
-  })
+    const data = [];
 
-  const maxValue = d3.max(data);
-  const yScale = d3.scaleLinear()
-    .domain([ 0, maxValue ])
-    .range([lineChartHeight - 40, 20]);
+    years.forEach((y, i) => {
+      data.push(pioggia[c.path][y]);
+    })
 
-  let path = [`M${getXPos(0)} ${yScale(pioggia[years[0]])}`];
-  
-  years.forEach((y, i) => {
-    const xp = getXPos(i);
-    const yp = yScale(pioggia[y]);
-    path.push(`L${xp} ${yp}`);
+    const maxValue = d3.max(data);
+    const yScale = d3.scaleLinear()
+      .domain([ 0, maxValue ])
+      .range([lineChartHeight - 40, 20]);
 
-    gD.append('circle')
-      .attr('cx', xp)
-      .attr('cy', yp)
-      .attr('r', 5)
-      .attr('stroke', colors.DEFAULT)
-      .attr('fill', colors.EMPTY)
-      .attr('class','line-dot')
-      .attr('data-year', y)
-      .on('mouseenter', selectYear)
-      .on('mouseleave', deselectYear);
+    let path = [`M${getXPos(0)} ${yScale(pioggia[c.path][years[0]])}`];
+    
+    years.forEach((y, i) => {
+      const xp = getXPos(i);
+      const yp = yScale(pioggia[c.path][y]);
+      path.push(`L${xp} ${yp}`);
 
-    gLB.append('text')
-      .attr('x', xp)
-      .attr('y', yp)
-      .attr('dy', -8)
-      .attr('class',`line-label ${i === 0 ? 'first' : ''}`)
-      .attr('data-year', y)
-      .attr('text-anchor', `${(i === years.length - 1 ? 'start' : (i === 0 ? 'end' : 'middle'))}`)
-      .text(`${formatNumber(pioggia[y])}mm`);
+      gD.append('circle')
+        .attr('cx', xp)
+        .attr('cy', yp)
+        .attr('r', 5)
+        .attr('stroke', colors.DEFAULT)
+        .attr('fill', colors.EMPTY)
+        .attr('class','line-dot')
+        .attr('data-year', y)
+        .on('mouseenter', selectYear)
+        .on('mouseleave', deselectYear);
 
-    gLB.append('text')
-      .attr('x', xp)
-      .attr('y', lineChartHeight - 20)
-      .attr('dy', 20)
-      .attr('class','line-axis-label')
-      .text(y);
+      gLB.append('text')
+        .attr('x', xp)
+        .attr('y', yp)
+        .attr('dy', -8)
+        .attr('class',`line-label ${i === 0 ? 'first' : ''}`)
+        .attr('data-year', y)
+        .attr('text-anchor', `${(i === years.length - 1 ? 'start' : (i === 0 ? 'end' : 'middle'))}`)
+        .text(`${formatNumber(pioggia[c.path][y])}${c.unit}`);
 
-    gG.append('line')
-      .attr('x1', xp)
-      .attr('x2', xp)
-      .attr('y1', lineChartHeight - 20)
-      .attr('y2', yp)
-      .attr('stroke', colors.DEFAULT)
-      .attr('class', 'line-guide');
-  });
-  gL.append('path')
-    .attr('d', path.join(','))
-    .attr('stroke', colors.NEUTRAL)
-    .attr('class', 'line-line');
+      gLB.append('text')
+        .attr('x', xp)
+        .attr('y', lineChartHeight - 20)
+        .attr('dy', 20)
+        .attr('class','line-axis-label')
+        .text(y);
 
-  const legend = d3.select('#rain')
-    .append('div')
+      gG.append('line')
+        .attr('x1', xp)
+        .attr('x2', xp)
+        .attr('y1', lineChartHeight - 20)
+        .attr('y2', yp)
+        .attr('stroke', colors.DEFAULT)
+        .attr('class', 'line-guide');
+    });
+    gL.append('path')
+      .attr('d', path.join(','))
+      .attr('stroke', colors.NEUTRAL)
+      .attr('class', 'line-line');
+
+    const legend = cc.append('div')
       .attr('class', 'line-legend');
-  
+    
     legend.append('div')
       .attr('class', 'legend')
-      .text('mm di Pioggia Annuali');
+      .html(c.title);
+  });
 };
